e7587fe7cbcb17deaf43de99ad472302140691f0
Charlie Harte <charlieharte@Charlies-MBP.attlocal.net>
2023-05-25 21:12:41 -0400

    uploading github demo

---
+++ src/Components/Display.js
@@ -0,0 +1,16 @@
+function Display ({ userData }) {
+  return (
+    <div>
+      <h1>{userData.name}</h1>
+      <img src={userData.avatar_url} alt='profile' />
+      <a href={userData.html_url} target='blank'>
+        <h3>Profile page</h3>
+      </a>
+      {userData.location && <h3>Location: {userData.location}</h3>}
+      {userData.email && <h3>Email: {userData.email}</h3>}
+      {userData.hireable && <h3>Hireable {userData.hireable}</h3>}
+    </div>
+  )
+}
+
+export default Display
---
+++ src/Components/Input.js
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+function Input ({ setUserData }) {
+  const [username, setUsername] = useState('')
+
+  const handleChange = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const res = await fetch(`https://api.github.com/users/${username}`)
+    const data = await res.json()
+    setUserData(data)
+    console.log(data)
+  }
+
+  const clear = () => {
+    setUserData(undefined)
+    setUsername('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input value={username} onChange={handleChange} placeholder='username' />
+        <input type='submit' />
+      </form>
+      <button onClick={clear}>Clear</button>
+
+    </div>
+  )
+}
+
+export default Input
